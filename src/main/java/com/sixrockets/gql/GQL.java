5ed35bb388060707eb06835e80a16a93481c6546
Dave Brown <d@sixrocketsstrong.com>
2020-04-20 11:17:04 -0600

    fixes issue with mapping operations

--- src/main/java/com/sixrockets/gql/GQL.java
+++ src/main/java/com/sixrockets/gql/GQL.java
@@ -1,21 +1,17 @@
 /*
-Licensed to the Apache Software Foundation (ASF) under one
-or more contributor license agreements.  See the NOTICE file
-distributed with this work for additional information
-regarding copyright ownership.  The ASF licenses this file
-to you under the Apache License, Version 2.0 (the
-"License"); you may not use this file except in compliance
-with the License.  You may obtain a copy of the License at
-
-  http://www.apache.org/licenses/LICENSE-2.0
-
-Unless required by applicable law or agreed to in writing,
-software distributed under the License is distributed on an
-"AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
-KIND, either express or implied.  See the License for the
-specific language governing permissions and limitations
-under the License.
-*/
+ * Licensed to the Apache Software Foundation (ASF) under one or more contributor license
+ * agreements. See the NOTICE file distributed with this work for additional information regarding
+ * copyright ownership. The ASF licenses this file to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance with the License. You may obtain a
+ * copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software distributed under the License
+ * is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express
+ * or implied. See the License for the specific language governing permissions and limitations under
+ * the License.
+ */
 package com.sixrockets.gql;
 
 import java.util.ArrayList;
@@ -49,7 +45,7 @@ public class GQL {
 	public GQL op(Operation operation) {
 		Operation op = operation.toBuilder().build();
 		op.parent = this;
-		operations.add(operation);
+		operations.add(op);
 		return this;
 	}
 
@@ -67,7 +63,7 @@ public class GQL {
 	public Operation opWith(Operation operation) {
 		Operation op = operation.toBuilder().build().cleanSelection();
 		op.parent = this;
-		operations.add(operation);
+		operations.add(op);
 		return op;
 	}
 
